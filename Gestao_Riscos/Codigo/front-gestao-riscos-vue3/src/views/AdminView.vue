e2e73944414098d37631b666892b56c1c69e4d16
Rogi Rocz <rogirocz@gmail.com>
2025-10-14 17:18:35 -0300

    Testando persistencia do pinia

--- Gestao_Riscos/Codigo/front-gestao-riscos-vue3/src/views/AdminView.vue
+++ Gestao_Riscos/Codigo/front-gestao-riscos-vue3/src/views/AdminView.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>Tela de Admin</div>
+	<div>Tela de Admin</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useUserStore, useAuthStore } from '@/store/userStore';
+
+const authStore = useAuthStore();
+const userStore = useUserStore();
+
+console.log('user: ', userStore, ' auth: ', authStore);
+</script>
 
 <style scoped></style>
